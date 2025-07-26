67fc7b6b53f998b82a0ee56eba0cc877b9bd7a4c
Albert <albertmabo001@gmail.com>
2025-07-26 14:56:22 +0545

    initial controller and route setup for articles

---
+++ src/Controllers/articles.js
@@ -0,0 +1,52 @@
+/**
+ * @desc    Get All Articles
+ * @route   GET /api/v1/articles
+ * @access  Public
+ */
+
+const getAllArticles = (req,res) =>{
+    
+}
+
+/**
+ * @desc    Create New Articles
+ * @route   POST /api/v1/articles
+ * @access  Public
+ */
+const createArticle = (req,res) =>{
+
+}
+
+/**
+ * @desc    Get Single Article
+ * @route   GET /api/v1/articles/:id
+ * @access  Public
+ */
+const getArticle = (req,res) =>{
+
+}
+/**
+ * @desc    Update Single Article
+ * @route   PATCH /api/v1/articles/:id
+ * @access  Public
+ */
+const updateArticle = (req,res) =>{
+
+}
+/**
+ * @desc    Delete Single Article
+ * @route   DELETE /api/v1/articles/:id
+ * @access  Public
+ */
+const deleteArticle = (req,res) =>{
+
+}
+
+
+module.exports = {
+    getAllArticles,
+    createArticle,
+    getArticle,
+    updateArticle,
+    deleteArticle
+}
